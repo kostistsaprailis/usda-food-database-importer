a14bc2e3a965443e40daa15cb34931349a68e8d7
Konstantinos Tsaprailis <kostis.tsaprailis@gmail.com>
2016-07-02 16:53:30 +0300

    adding data import sql script

--- imports.sql
+++ imports.sql
@@ -1,16 +1,16 @@
 BEGIN;
 
-COPY foodgroups             FROM './FD_GROUP.psv' (DELIMITER '|');
-COPY foods                  FROM './FOOD_DES.psv' (DELIMITER '|');
-COPY langual_desc           FROM './LANGDESC.psv' (DELIMITER '|');
-COPY langual_factor         FROM './LANGUAL.psv'  (DELIMITER '|');
-COPY nutrients_desc         FROM './NUTR_DEF.psv' (DELIMITER '|');
-COPY source_codes           FROM './SRC_CD.psv'   (DELIMITER '|');
-COPY data_derivation_codes  FROM './DERIV_CD.psv' (DELIMITER '|');
-COPY weights                FROM './WEIGHT.psv'   (DELIMITER '|');
-COPY footnotes              FROM './FOOTNOTE.psv' (DELIMITER '|');
-COPY data_sources           FROM './DATA_SRC.psv' (DELIMITER '|');
-COPY data_sources_link      FROM './DATSRCLN.psv' (DELIMITER '|');
-COPY nutrients              FROM './NUT_DATA.psv' (DELIMITER '|');
+COPY foodgroups             FROM './FD_GROUP.psv' DELIMITER as '|' NULL as '';
+COPY foods                  FROM './FOOD_DES.psv' DELIMITER as '|' NULL as '';
+COPY langual_desc           FROM './LANGDESC.psv' DELIMITER as '|' NULL as '';
+COPY langual_factor         FROM './LANGUAL.psv'  DELIMITER as '|' NULL as '';
+COPY nutrients_desc         FROM './NUTR_DEF.psv' DELIMITER as '|' NULL as '';
+COPY source_codes           FROM './SRC_CD.psv'   DELIMITER as '|' NULL as '';
+COPY data_derivation_codes  FROM './DERIV_CD.psv' DELIMITER as '|' NULL as '';
+COPY weights                FROM './WEIGHT.psv'   DELIMITER as '|' NULL as '';
+COPY footnotes              FROM './FOOTNOTE.psv' DELIMITER as '|' NULL as '';
+COPY data_sources           FROM './DATA_SRC.psv' DELIMITER as '|' NULL as '';
+COPY data_sources_link      FROM './DATSRCLN.psv' DELIMITER as '|' NULL as '';
+COPY nutrients              FROM './NUT_DATA.psv' DELIMITER as '|' NULL as '';
 
 COMMIT;
