18293e28de71bf648db0514e377c5a176856f879
Konstantinos Tsaprailis <kostis.tsaprailis@gmail.com>
2016-07-02 21:22:48 +0300

    fixing file paths used in SQL scripts

--- imports.sql
+++ imports.sql
@@ -1,16 +1,16 @@
 BEGIN;
 
-COPY foodgroups             FROM './FD_GROUP.psv' DELIMITER as '|' NULL as '';
-COPY foods                  FROM './FOOD_DES.psv' DELIMITER as '|' NULL as '';
-COPY langual_desc           FROM './LANGDESC.psv' DELIMITER as '|' NULL as '';
-COPY langual_factor         FROM './LANGUAL.psv'  DELIMITER as '|' NULL as '';
-COPY nutrients_desc         FROM './NUTR_DEF.psv' DELIMITER as '|' NULL as '';
-COPY source_codes           FROM './SRC_CD.psv'   DELIMITER as '|' NULL as '';
-COPY data_derivation_codes  FROM './DERIV_CD.psv' DELIMITER as '|' NULL as '';
-COPY weights                FROM './WEIGHT.psv'   DELIMITER as '|' NULL as '';
-COPY footnotes              FROM './FOOTNOTE.psv' DELIMITER as '|' NULL as '';
-COPY data_sources           FROM './DATA_SRC.psv' DELIMITER as '|' NULL as '';
-COPY data_sources_link      FROM './DATSRCLN.psv' DELIMITER as '|' NULL as '';
-COPY nutrients              FROM './NUT_DATA.psv' DELIMITER as '|' NULL as '';
+COPY foodgroups             FROM '/tmp/FD_GROUP.psv' DELIMITER as '|' NULL as '';
+COPY foods                  FROM '/tmp/FOOD_DES.psv' DELIMITER as '|' NULL as '';
+COPY langual_desc           FROM '/tmp/LANGDESC.psv' DELIMITER as '|' NULL as '';
+COPY langual_factor         FROM '/tmp/LANGUAL.psv'  DELIMITER as '|' NULL as '';
+COPY nutrients_desc         FROM '/tmp/NUTR_DEF.psv' DELIMITER as '|' NULL as '';
+COPY source_codes           FROM '/tmp/SRC_CD.psv'   DELIMITER as '|' NULL as '';
+COPY data_derivation_codes  FROM '/tmp/DERIV_CD.psv' DELIMITER as '|' NULL as '';
+COPY weights                FROM '/tmp/WEIGHT.psv'   DELIMITER as '|' NULL as '';
+COPY footnotes              FROM '/tmp/FOOTNOTE.psv' DELIMITER as '|' NULL as '';
+COPY data_sources           FROM '/tmp/DATA_SRC.psv' DELIMITER as '|' NULL as '';
+COPY data_sources_link      FROM '/tmp/DATSRCLN.psv' DELIMITER as '|' NULL as '';
+COPY nutrients              FROM '/tmp/NUT_DATA.psv' DELIMITER as '|' NULL as '';
 
 COMMIT;
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# Requires unzip
 
 # download the USDA nutritional data
 wget https://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR/SR28/dnload/sr28asc.zip
@@ -7,18 +9,18 @@ wget https://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR/SR28/dnload/sr
 unzip sr28asc.zip
 
 # Transform data to psv (pipe separated values)
-python3 data-import.py -i FD_GROUP.txt
-python3 data-import.py -i FOOD_DES.txt
-python3 data-import.py -i LANGDESC.txt
-python3 data-import.py -i LANGUAL.txt
-python3 data-import.py -i NUTR_DEF.txt
-python3 data-import.py -i SRC_CD.txt
-python3 data-import.py -i DERIV_CD.txt
-python3 data-import.py -i WEIGHT.txt
-python3 data-import.py -i FOOTNOTE.txt
-python3 data-import.py -i DATA_SRC.txt
-python3 data-import.py -i DATSRCLN.txt
-python3 data-import.py -i NUT_DATA.txt
+python3 data-import.py -i FD_GROUP.txt  -o /tmp/FD_GROUP.psv
+python3 data-import.py -i FOOD_DES.txt  -o /tmp/FOOD_DES.psv
+python3 data-import.py -i LANGDESC.txt  -o /tmp/LANGDESC.psv
+python3 data-import.py -i LANGUAL.txt   -o /tmp/LANGUAL.psv
+python3 data-import.py -i NUTR_DEF.txt  -o /tmp/NUTR_DEF.psv
+python3 data-import.py -i SRC_CD.txt    -o /tmp/SRC_CD.psv
+python3 data-import.py -i DERIV_CD.txt  -o /tmp/DERIV_CD.psv
+python3 data-import.py -i WEIGHT.txt    -o /tmp/WEIGHT.psv
+python3 data-import.py -i FOOTNOTE.txt  -o /tmp/FOOTNOTE.psv
+python3 data-import.py -i DATA_SRC.txt  -o /tmp/DATA_SRC.psv
+python3 data-import.py -i DATSRCLN.txt  -o /tmp/DATSRCLN.psv
+python3 data-import.py -i NUT_DATA.txt  -o /tmp/NUT_DATA.psv
 
 #C reate the DB schema. Make sure the DB and name/role exist otherwise this will fail.
 psql -f schema.sql
@@ -26,5 +28,6 @@ psql -f schema.sql
 # Import the data
 psql -f imports.sql
 
-# Delete all downloaded files once we're finished
-#ls *.psv *.txt *.pdf *.zip | xargs rm
+# Delete all relevant files once we're finished
+ls *.txt *.pdf *.zip | xargs rm
+ls *.psv | xargs rm
